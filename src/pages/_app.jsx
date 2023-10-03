02952321e81471a3f57e51085da324e4f786bbeb
ukssss <1872003115su@gmail.com>
2023-10-03 21:49:04 +0900

    Refactor | _app.jsx 구조 변경
    - ThemeProvider, GlobalStyle 제거

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,5 @@
-import { ThemeProvider } from "styled-components";
-import GlobalStyle from "@/styles/globalStyle";
-import { darkTheme } from "@/styles/theme";
-
 const App = ({ Component, pageProps }) => {
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default App;
