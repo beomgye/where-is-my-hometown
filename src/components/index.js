74e5a5937520f11aaf8e45d4cc4c0bb9e36ace49
ukssss <1872003115su@gmail.com>
2023-10-04 20:37:43 +0900

    Feat | Input 컴포넌트 제작

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Header } from './common/Header'
 export { default as Form } from './common/Form'
 export { default as Button } from './common/Button'
+export { default as Input } from './common/Input'
 
 export { default as AssetInputForm } from './AssetInputForm'
 export { default as BuildingTypeForm } from './BuildingTypeForm'
