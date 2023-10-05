08c2d3fce1efae3b47d26a84d396c102daa433d1
ukssss <1872003115su@gmail.com>
2023-10-05 23:51:55 +0900

    Feat | OptionContainer step 을 통한 옵션 선택창 변경 구현

--- src/containers/OptionContainer/index.jsx
+++ src/containers/OptionContainer/index.jsx
@@ -1,22 +1,41 @@
-import { useState } from "react";
+import { useCallback, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import {
   AssetInputForm,
-  LocationForm,
-  TradeForm,
   BuildingTypeForm,
   FinishForm,
-} from "@/components";
+  LocationForm,
+  TradeForm
+} from '@/components';
 
 const OptionContainer = () => {
-  const [step] = useState(0);
+  const { control, handleSubmit } = useForm();
+  const [step, setStep] = useState(0);
+
+  const onGoBack = useCallback(() => {
+    setStep(step - 1);
+  }, [step]);
+
+  const onSubmit = useCallback(() => {
+    const nextStep = step + 1;
+    setStep(nextStep);
+  }, [step]);
 
   return (
     <div>
-      {step === 0 && <AssetInputForm />}
-      {step === 1 && <LocationForm />}
-      {step === 2 && <TradeForm />}
-      {step === 3 && <BuildingTypeForm />}
-      {step === 4 && <FinishForm />}
+      {step === 0 && <AssetInputForm control={control} onSubmit={handleSubmit(onSubmit)} />}
+      {step === 1 && (
+        <LocationForm control={control} onSubmit={handleSubmit(onSubmit)} onGoBack={onGoBack} />
+      )}
+      {step === 2 && (
+        <TradeForm control={control} onSubmit={handleSubmit(onSubmit)} onGoBack={onGoBack} />
+      )}
+      {step === 3 && (
+        <BuildingTypeForm control={control} onSubmit={handleSubmit(onSubmit)} onGoBack={onGoBack} />
+      )}
+      {step === 4 && (
+        <FinishForm control={control} onSubmit={handleSubmit(onSubmit)} onGoBack={onGoBack} />
+      )}
     </div>
   );
 };
