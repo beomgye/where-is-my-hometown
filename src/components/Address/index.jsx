bc26fd22a592edef4fea2cfd84c69d47aa91830e
Seung taek Oh <elqkddl@naver.com>
2023-10-06 15:47:03 +0900

    Feat |  map 컴포넌트 받아오기 #12

--- src/components/Address/index.jsx
+++ src/components/Address/index.jsx
@@ -1,37 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DaumPostcode from "react-daum-postcode";
 import Modal from "react-modal";
+import customStyles from "@/utils/customStyles";
+import Map from "../Map";
 
 const Address = () => {
   const [roadAddress, setRoadAddress] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+  const [geocoder, setGeocoder] = useState(null);
+  const [marker, setMarker] = useState(null);
+
+  useEffect(() => {
+    if (geocoder && marker) {
+      document.getElementById("address").value = roadAddress;
+
+      geocoder.addressSearch(roadAddress, function (results, status) {
+        if (status === window.daum.maps.services.Status.OK) {
+          const result = results[0];
+          const coords = new window.daum.maps.LatLng(result.y, result.x);
+
+          document.getElementById("map").style.display = "block";
+
+          window.map.relayout();
+          window.map.setCenter(coords);
+
+          marker.setPosition(coords);
+        }
+      });
+    }
+  }, [geocoder, marker, roadAddress]);
+
   const completeHandler = (data) => {
     console.log(data);
     setRoadAddress(data.roadAddress);
     setIsOpen(false);
   };
 
-  const customStyles = {
-    overlay: {
-      backgroundColor: "rgba(0,0,0,0.5)",
-    },
-    content: {
-      left: "0",
-      margin: "auto",
-      width: "500px",
-      height: "600px",
-      padding: "0",
-      overflow: "hidden",
-    },
-  };
-
   const OnToggle = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleGeocoderLoad = (Mapgeocoder) => {
+    setGeocoder(Mapgeocoder);
+  };
+
+  const handleMarkerLoad = (MapMarker) => {
+    setMarker(MapMarker);
+  };
+
   return (
     <div>
-      <input type="text" value={roadAddress} placeholder="도로명주소" readOnly />
+      <Map onGeocoderLoad={handleGeocoderLoad} onMarkerLoad={handleMarkerLoad} />
+      <input type="text" id="address" value={roadAddress} placeholder="도로명주소" readOnly />
       <button type="button" onClick={OnToggle}>
         우편번호 검색
       </button>
