9ef4736a383890d9e8bea9bf85ac078fa6f2d9fe
ukssss <1872003115su@gmail.com>
2023-10-10 20:18:09 +0900

    Fix | Checkbox 컴포넌트 절대경로 설정

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as NavBar } from './common/NavBar';
 export { default as Form } from './common/Form';
 export { default as Button } from './common/Button';
 export { default as Input } from './common/Input';
+export { default as Checkbox } from './common/Checkbox';
 
 export { default as AssetInputForm } from './AssetInputForm';
 export { default as LocationForm } from './LocationForm';
