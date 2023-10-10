2a8c8c9710ff5730f0f579f6fe7a378c393e7570
ukssss <1872003115su@gmail.com>
2023-10-10 20:20:34 +0900

    Feat | types 추가

---
+++ src/types/plan.js
@@ -0,0 +1,47 @@
+export const TradeType = [
+  {
+    id: 1,
+    name: '전세'
+  },
+  {
+    id: 2,
+    name: '월세'
+  },
+  {
+    id: 3,
+    name: '매매'
+  }
+];
+
+export const BuildingType = [
+  {
+    id: 1,
+    name: '아파트'
+  },
+  {
+    id: 2,
+    name: '연립'
+  },
+  {
+    id: 3,
+    name: '단독'
+  },
+  {
+    id: 4,
+    name: '오피스텔'
+  }
+];
+
+export const TradeTypeOptions = TradeType.map(({ id, name: label }) => {
+  return {
+    id,
+    label
+  };
+});
+
+export const BuildingTypeOptions = BuildingType.map(({ id, name: label }) => {
+  return {
+    id,
+    label
+  };
+});
