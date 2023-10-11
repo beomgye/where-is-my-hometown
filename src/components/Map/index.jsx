6618832cf643150b9d76e339fa89547b8bb6aaf9
Seung taek Oh <elqkddl@naver.com>
2023-10-11 21:17:41 +0900

    Feat | Map 컴포넌트 임시 해제 #13

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -3,7 +3,11 @@ import { Map } from "react-kakao-maps-sdk";
 const KakaoMap = () => {
   return (
     <>
-      <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "100%", height: "360px" }} />
+      <Map
+        center={{ lat: 33.5563, lng: 126.79581 }}
+        style={{ width: "350px", height: "200px" }}
+        level={3}
+      />
       <div>Test</div>
     </>
   );
