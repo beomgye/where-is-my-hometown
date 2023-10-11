308d2d0211c5c6c3ef43a34f19164f490380e317
Seung taek Oh <elqkddl@naver.com>
2023-10-11 23:07:43 +0900

    Feat | 불필요한 State 지움으로써 Map렌더링 해결 완료 #12

--- src/components/Address/index.jsx
+++ src/components/Address/index.jsx
@@ -3,23 +3,10 @@ import DaumPostcode from "react-daum-postcode";
 import Modal from "react-modal";
 import { CustomOverlayMap, Map, MapMarker } from "react-kakao-maps-sdk";
 
-// import KakaoMap from "../Map";
-
 const Address = () => {
-  const [state, setState] = useState({
-    // 지도의 초기 위치
-    center: { lat: 33.452613, lng: 126.570888 },
-    // 부드럽게 이동할 지 유무
-    isPanto: true,
-    // 검색한 주소
-    address: "",
-    // 지도 레벨
-    level: 3,
-  });
-
+  const [center, setCenter] = useState({ lat: 33.452613, lng: 126.570888 });
   const [address, setAddress] = useState("제주 Kakao 스페이드닷투");
   const [isModalOpen, isSetModalOpen] = useState(false);
-  // const [submit, setSubmit] = useState(false); // 임시
 
   const completeHandler = (data) => {
     console.log(data);
@@ -36,15 +23,9 @@ const Address = () => {
       geocoder.addressSearch(address, function (result, status) {
         // 정상적으로 검색이 완료됐으면
         if (status === window.kakao.maps.services.Status.OK) {
-          setState((prev) => {
-            return {
-              ...prev,
-              center: {
-                lat: Number(result[0].y),
-                lng: Number(result[0].x),
-              },
-              isPanto: !prev.isPanto,
-            };
+          setCenter({
+            lat: Number(result[0].y),
+            lng: Number(result[0].x),
           });
         }
       });
@@ -56,30 +37,21 @@ const Address = () => {
   }, [address]);
 
   const onClick = () => {
-    setState((prev) => {
-      return { ...prev, address };
-    });
-
     isSetModalOpen(!isModalOpen);
   };
 
   return (
     <div>
-      <Map
-        style={{ width: "350px", height: "200px" }}
-        center={state.center}
-        isPanto={state.isPanto}
-        level={state.level}
-      >
-        <MapMarker position={state.center} />
-        <CustomOverlayMap position={state.center} yAnchor={1}>
+      <Map style={{ width: "350px", height: "200px" }} center={center} isPanto level={3}>
+        <MapMarker position={center} />
+        <CustomOverlayMap position={center} yAnchor={1}>
           <div>
             <a
-              href={`https//maps.kakao.com/link/map/${state.address},${state.center.lat},${state.center.lng}`}
+              href={`https//maps.kakao.com/link/map/${address},${center.lat},${center.lng}`}
               target="_blank"
               rel="noreferrer"
             >
-              <span>{state.address}</span>
+              <span>{address}</span>
             </a>
           </div>
         </CustomOverlayMap>
