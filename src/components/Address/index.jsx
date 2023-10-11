feed7adc4ae15e2ae26d30ad68bf3395946455ce
Seung taek Oh <elqkddl@naver.com>
2023-10-11 21:18:31 +0900

    Bug | 버튼을 두번 눌러야 현재 위치 맵에 표시

--- src/components/Address/index.jsx
+++ src/components/Address/index.jsx
@@ -1,63 +1,105 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import DaumPostcode from "react-daum-postcode";
 import Modal from "react-modal";
-import customStyles from "@/utils/customStyles";
-import Map from "../Map";
+import { CustomOverlayMap, Map, MapMarker } from "react-kakao-maps-sdk";
+
+// import KakaoMap from "../Map";
 
 const Address = () => {
-  const [roadAddress, setRoadAddress] = useState("");
+  const [state, setState] = useState({
+    // 지도의 초기 위치
+    center: { lat: 33.452613, lng: 126.570888 },
+    // 부드럽게 이동할 지 유무
+    isPanto: true,
+    // 검색한 주소
+    address: "경기도 안양시 동안구 1143-3 ",
+    // 지도 레벨
+    level: 3,
+  });
+  const [address, setAddress] = useState("");
   const [isOpen, setIsOpen] = useState(false);
-  const [geocoder, setGeocoder] = useState(null);
-  const [marker, setMarker] = useState(null);
-
-  useEffect(() => {
-    if (geocoder && marker) {
-      document.getElementById("address").value = roadAddress;
-
-      geocoder.addressSearch(roadAddress, function (results, status) {
-        if (status === window.daum.maps.services.Status.OK) {
-          const result = results[0];
-          const coords = new window.daum.maps.LatLng(result.y, result.x);
-
-          document.getElementById("map").style.display = "block";
-
-          window.map.relayout();
-          window.map.setCenter(coords);
-
-          marker.setPosition(coords);
-        }
-      });
-    }
-  }, [geocoder, marker, roadAddress]);
+  const [submit, setSubmit] = useState(false); // 임시
 
   const completeHandler = (data) => {
     console.log(data);
-    setRoadAddress(data.roadAddress);
+    setAddress(data.roadAddress);
     setIsOpen(false);
   };
 
-  const OnToggle = () => {
+  const kakaoMapGeoCoder = () => {
+    window.kakao.maps.load(() => {
+      // 주소-좌표 변환 객체를 생성합니다
+      const geocoder = new window.kakao.maps.services.Geocoder();
+
+      // 주소로 좌표를 검색합니다
+      geocoder.addressSearch(address, function (result, status) {
+        // 정상적으로 검색이 완료됐으면
+        if (status === window.kakao.maps.services.Status.OK) {
+          setState((prev) => {
+            return {
+              ...prev,
+              center: {
+                lat: Number(result[0].y),
+                lng: Number(result[0].x),
+              },
+              isPanto: !prev.isPanto,
+            };
+          });
+        }
+      });
+    });
+  };
+
+  useEffect(() => {
+    kakaoMapGeoCoder();
+    setSubmit(false);
+  }, [submit]);
+
+  const onClick = () => {
     setIsOpen(!isOpen);
-  };
-
-  const handleGeocoderLoad = (Mapgeocoder) => {
-    setGeocoder(Mapgeocoder);
-  };
-
-  const handleMarkerLoad = (MapMarker) => {
-    setMarker(MapMarker);
+    setState((prev) => {
+      return { ...prev, address };
+    });
+    setSubmit(true);
   };
 
   return (
     <div>
-      <Map onGeocoderLoad={handleGeocoderLoad} onMarkerLoad={handleMarkerLoad} />
-      <input type="text" id="address" value={roadAddress} placeholder="도로명주소" readOnly />
-      <button type="button" onClick={OnToggle}>
-        우편번호
-      </button>
-      <Modal isOpen={isOpen} ariaHideApp={false} style={customStyles}>
-        <DaumPostcode onComplete={completeHandler} />
-      </Modal>
+      <Map
+        style={{ width: "350px", height: "200px" }}
+        center={state.center}
+        isPanto={state.isPanto}
+        level={state.level}
+      >
+        <MapMarker position={state.center} />
+        <CustomOverlayMap position={state.center} yAnchor={1}>
+          <div>
+            <a
+              href={`https//maps.kakao.com/link/map/${state.address},${state.center.lat},${state.center.lng}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span>{state.address}</span>
+            </a>
+          </div>
+        </CustomOverlayMap>
+      </Map>
+      <div>
+        <p>주소를 입력하세요</p>
+        <input
+          type="text"
+          value={address}
+          onChange={(e) => {
+            setAddress(e.target.value);
+          }}
+        />
+        <button type="button" onClick={onClick}>
+          우편번호 검색
+        </button>
+        <Modal isOpen={isOpen} ariaHideApp={false}>
+          <DaumPostcode onComplete={completeHandler} />
+        </Modal>
+      </div>
     </div>
   );
 };
