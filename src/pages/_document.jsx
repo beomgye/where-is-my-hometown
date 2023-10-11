3cca37d6a2e0ef3d05008b4b9e75351cfd403b00
Seung taek Oh <elqkddl@naver.com>
2023-10-11 14:17:30 +0900

    [Fix] Script 추가

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,7 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+
 import Script from "next/script";
 import { Header } from "@/components";
 
+const API = process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY;
+const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${API}&libraries=services,clusterer&autoload=false`;
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const originalRenderPage = ctx.renderPage;
@@ -21,15 +24,12 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <Script
-            type="text/javascript"
-            src="//dapi.kakao.com/v2/maps/sdk.js?appkey=d248939062099475f9c7ced600dbbbc6"
-          />
           <Header />
         </Head>
         <body>
           <Main />
           <NextScript />
+          <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
         </body>
       </Html>
     );
