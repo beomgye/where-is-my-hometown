fe9567f797a51e643dfc49301700148a7183e664
Seung taek Oh <elqkddl@naver.com>
2023-10-11 14:18:08 +0900

    Feat | react-kakao-maps-sdk 이용한 맵 랜더링 성공 #13

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,34 +1,12 @@
-import { useEffect } from "react";
+import { Map } from "react-kakao-maps-sdk";
 
-const Map = ({ onGeocoderLoad, onMarkerLoad }) => {
-  useEffect(() => {
-    const kakaoMapScript = document.createElement("script");
-    kakaoMapScript.async = false;
-    kakaoMapScript.src =
-      "//dapi.kakao.com/v2/maps/sdk.js?appkey=d248939062099475f9c7ced600dbbbc6&libraries=services&autoload=false";
-    document.head.appendChild(kakaoMapScript);
-
-    const onLoadKakaoAPI = () => {
-      window.kakao.maps.load(() => {
-        const container = document.getElementById("map");
-        const options = {
-          center: new window.kakao.maps.LatLng(37.537187, 127.005476),
-          level: 3,
-        };
-        const map = new window.kakao.maps.Map(container, options);
-        const geocoder = new window.kakao.maps.services.Geocoder();
-        const marker = new window.kakao.maps.Marker({
-          position: new window.kakao.maps.LatLng(37.537187, 127.005476),
-          map,
-        });
-        onGeocoderLoad(geocoder);
-        onMarkerLoad(marker);
-      });
-    };
-    kakaoMapScript.addEventListener("load", onLoadKakaoAPI);
-  }, []);
-
-  return <div style={{ width: "300px", height: "300px", marginTop: "10px" }} id="map" />;
+const KakaoMap = () => {
+  return (
+    <>
+      <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "100%", height: "360px" }} />
+      <div>Test</div>
+    </>
+  );
 };
 
-export default Map;
+export default KakaoMap;
