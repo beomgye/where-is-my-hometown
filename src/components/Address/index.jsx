4e0268e7050f4365f1f0cf6d9d8a7e8b0702d457
Seung taek Oh <elqkddl@naver.com>
2023-10-13 15:12:42 +0900

    Design | CSS 적용 #12

--- src/components/Address/index.jsx
+++ src/components/Address/index.jsx
@@ -1,11 +1,14 @@
-import { useState, useEffect } from "react";
-import DaumPostcode from "react-daum-postcode";
-import Modal from "react-modal";
-import { CustomOverlayMap, Map, MapMarker } from "react-kakao-maps-sdk";
+import { useState, useEffect } from 'react';
+import DaumPostcode from 'react-daum-postcode';
+import Modal from 'react-modal';
+import { CustomOverlayMap, Map, MapMarker } from 'react-kakao-maps-sdk';
+import styled from 'styled-components';
+import ModalCustomStyles from '@/utils/customStyles';
+import { grayColor } from '@/styles/variables';
 
 const Address = () => {
   const [center, setCenter] = useState({ lat: 33.452613, lng: 126.570888 });
-  const [address, setAddress] = useState("제주 Kakao 스페이드닷투");
+  const [address, setAddress] = useState('');
   const [isModalOpen, isSetModalOpen] = useState(false);
 
   const completeHandler = (data) => {
@@ -25,7 +28,7 @@ const Address = () => {
         if (status === window.kakao.maps.services.Status.OK) {
           setCenter({
             lat: Number(result[0].y),
-            lng: Number(result[0].x),
+            lng: Number(result[0].x)
           });
         }
       });
@@ -42,38 +45,56 @@ const Address = () => {
 
   return (
     <div>
-      <Map style={{ width: "350px", height: "200px" }} center={center} isPanto level={3}>
-        <MapMarker position={center} />
-        <CustomOverlayMap position={center} yAnchor={1}>
-          <div>
-            <a
-              href={`https//maps.kakao.com/link/map/${address},${center.lat},${center.lng}`}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span>{address}</span>
-            </a>
-          </div>
-        </CustomOverlayMap>
+      <Map style={{ width: '450px', height: '250px' }} center={center} isPanto level={3}>
+        <MapMarker position={center}>
+          <InfoWindow>{address}</InfoWindow>
+          <CustomOverlayMap position={center} yAnchor={1} />
+        </MapMarker>
       </Map>
-      <div>
-        <p>주소를 입력하세요</p>
-        <input
-          type="text"
-          value={address}
-          onChange={(e) => {
-            setAddress(e.target.value);
-          }}
-        />
-        <button type="button" onClick={onClick}>
-          우편번호 검색
-        </button>
-        <Modal isOpen={isModalOpen} ariaHideApp={false}>
-          <DaumPostcode onComplete={completeHandler} />
-        </Modal>
-      </div>
+      <AddressContainer>
+        <AddressInput type="text" value={address} readOnly />
+        <AddressButton type="button" onClick={onClick} value="장소 선택" />
+      </AddressContainer>
+      <Modal isOpen={isModalOpen} ariaHideApp={false} style={ModalCustomStyles}>
+        <DaumPostcode onComplete={completeHandler} />
+      </Modal>
     </div>
   );
 };
+
+const InfoWindow = styled.div`
+  padding: 5px;
+  width: 200px;
+  height: 100%;
+`;
+
+const AddressContainer = styled.div`
+  position: relative;
+  background-color: red;
+  margin-top: 45px;
+`;
+
+const AddressInput = styled.input`
+  align-items: center;
+  width: 450px;
+  height: 25px;
+  border-right: 0px;
+  border-left: 0px;
+  border-top: 0px;
+`;
+
+const AddressButton = styled.input`
+  width: 68px;
+  height: 14px;
+  border: 0;
+  margin: auto 0;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: -10px;
+  background-color: white;
+  color: ${grayColor};
+  cursor: pointer;
+`;
 
 export default Address;
