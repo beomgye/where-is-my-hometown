35f43512b3fa65780c6cd33e95e0caa5e28396d8
ukssss <1872003115su@gmail.com>
2023-10-15 00:33:24 +0900

    Fix | 초기 페이지 렌더링시 FOUC 문제 해결

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,23 +1,33 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 import Script from 'next/script';
+import React from 'react';
 import { Header } from '@/components';
 
 const API = process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY;
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${API}&libraries=services,clusterer&autoload=false`;
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => App,
-        enhanceComponent: (Component) => Component
-      });
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+        });
 
-    const initialProps = await Document.getInitialProps(ctx);
+      const initialProps = await Document.getInitialProps(ctx);
 
-    return initialProps;
+      return {
+        ...initialProps,
+        styles: [...React.Children.toArray(initialProps.styles), sheet.getStyleElement()]
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
