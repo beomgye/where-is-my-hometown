a5c060a471cc8682a609dc95fe06ac8f52359a1f
ukssss <1872003115su@gmail.com>
2023-10-17 02:21:27 +0900

    Refactor | constants 데이터 변수 리팩토링

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,36 +1,81 @@
-export const Step = Object.freeze({
-  ASSET: 0,
-  LOCATION: 1,
-  TRANSACTION: 2,
-  BUILDING: 3,
-  SUMMARY: 4,
-  FINAL: 5
+export const Step = [
+  {
+    id: 0,
+    value: '자산 입력'
+  },
+  {
+    id: 1,
+    value: '위치 선택'
+  },
+  {
+    id: 2,
+    value: '거래 방식'
+  },
+  {
+    id: 3,
+    value: '건물 유형'
+  },
+  {
+    id: 4,
+    value: '마무리 단계'
+  },
+  {
+    id: 5,
+    value: '결과'
+  }
+];
+
+export const Transaction = [
+  {
+    id: 0,
+    value: '월세'
+  },
+  {
+    id: 1,
+    value: '전세'
+  },
+  {
+    id: 2,
+    value: '매매'
+  }
+];
+
+export const Building = [
+  {
+    id: 0,
+    value: '아파트'
+  },
+  {
+    id: 1,
+    value: '연립'
+  },
+  {
+    id: 2,
+    value: '단독'
+  },
+  {
+    id: 3,
+    value: '주택종합'
+  }
+];
+
+export const StepOptions = Step.map(({ id, value }) => {
+  return {
+    id,
+    value
+  };
 });
 
-export const Transaction = Object.freeze({
-  MONTHLY: 0,
-  JEONSE: 1,
-  SALE: 2
+export const TransactionTypeOptions = Transaction.map(({ id, value }) => {
+  return {
+    id,
+    value
+  };
 });
 
-export const Building = Object.freeze({
-  APRARTMENT: 0,
-  TOWNHOUSE: 1,
-  SINGLE_FAMILY_HOME: 2,
-  HOUSING_COMPLEX: 3
+export const BuildingTypeOptions = Building.map(({ id, value }) => {
+  return {
+    id,
+    value
+  };
 });
-
-export const StepOptions = Object.entries(Step).map(([step, index]) => ({
-  step,
-  index
-}));
-
-export const TransactionTypeOptions = Object.entries(Transaction).map(([label, value]) => ({
-  label,
-  value
-}));
-
-export const BuildingTypeOptions = Object.entries(Building).map(([label, value]) => ({
-  label,
-  value
-}));
